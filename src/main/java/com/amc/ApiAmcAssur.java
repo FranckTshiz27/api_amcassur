5e8617105778511a12da9baac7e14b12a1849908
GASPARD PETEMOYA <gpetemoya@local.rawsur.com>
2024-08-31 12:43:10 +0100

    config for amc

--- src/main/java/com/amc/ApiAmcAssur.java
+++ src/main/java/com/amc/ApiAmcAssur.java
@@ -6,28 +6,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import lombok.extern.slf4j.Slf4j;
 
-
 @Slf4j
 @SpringBootApplication
 public class ApiAmcAssur implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiAmcAssur.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAmcAssur.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		log.info("Server run");
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Server run");
 
-		// CKeyclaokCredentialApi keyclaok = new CKeyclaokCredentialApi();
-		// keyclaok.setId("63e12c6348c1786c7ce3638d");
-		// keyclaok.setUsername("admin");
-		// // keyclaok.setUsername("kevin");
-		// keyclaok.setPassword("Rawsur2022$");
-		// // keyclaok.setPassword("12345678");
-		// keyclaok.setClientid("admin-cli");
-		// keyclaok.setGranttype("password");
-		// this.accountService.getToken(keyclaok);
-	}
+        // CKeyclaokCredentialApi keyclaok = new CKeyclaokCredentialApi();
+        // keyclaok.setId("63e12c6348c1786c7ce3638d");
+        // keyclaok.setUsername("admin");
+        // // keyclaok.setUsername("kevin");
+        // keyclaok.setPassword("Rawsur2022$");
+        // // keyclaok.setPassword("12345678");
+        // keyclaok.setClientid("admin-cli");
+        // keyclaok.setGranttype("password");
+        // this.accountService.getToken(keyclaok);
+    }
 
 }
